ce511c43b87d1f0ee3aa8036f51671285c690c57
Chris Connors <c.connors@gmail.com>
2020-10-27 22:24:36 -0400

    added time_enums.py for categories of entries into timeblock

--- time_block.py
+++ time_block.py
@@ -1,3 +1,24 @@
+import time
+from time_enums import TimeBlockCategories
 from ntp_time import ntp_time
 
-print(ntp_time())
+#print(time.ctime(ntp_time()).replace("  ", " "))
+
+class TimeBlock:
+
+    currentTime = time.ctime(ntp_time())
+    category = TimeBlockCategories.MISC
+
+
+
+
+
+def main():
+
+    block1 = TimeBlock
+
+    print(TimeBlock.category.value)
+
+
+if __name__ == "__main__":
+    main()
---
+++ time_enums.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class TimeBlockCategories(Enum):
+    GENERAL = 0
+    SPORTS = 1
+    POLITICS = 2
+    TECH = 3
+    BUSINESS = 4
+    LOCAL = 5
+    MISC = 6
+
