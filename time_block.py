eedc49e79284bb420b33f2c40057e8b33611a718
Chris Connors <c.connors@gmail.com>
2020-10-27 22:33:47 -0400

    added Entry class to time_block.py

--- time_block.py
+++ time_block.py
@@ -2,22 +2,44 @@ import time
 from time_enums import TimeBlockCategories
 from ntp_time import ntp_time
 
-#print(time.ctime(ntp_time()).replace("  ", " "))
+
+class Entry:
+
+    category = TimeBlockCategories.MISC
+    entry_time = time.ctime(ntp_time())
+    entry_url = ''
+    ipfs_id = ''
+    location = ''
+
+    def __init__(self, category, url):
+        self.category = TimeBlockCategories(category)
+        self.url = url
+
+    def get_url(self):
+        return self.url
+
+    def get_time(self):
+        return self.entry_time
+
+    def get_ipfs_id(self):
+        return self.ipfs_id
+
+    def get_location(self):
+        return self.location
+
+
 
 class TimeBlock:
 
     currentTime = time.ctime(ntp_time())
-    category = TimeBlockCategories.MISC
-
-
-
+    entries = []
 
 
 def main():
 
     block1 = TimeBlock
 
-    print(TimeBlock.category.value)
+    # print(time.ctime(ntp_time()).replace("  ", " "))
 
 
 if __name__ == "__main__":
