e767d2a1022eb151ed7889a1704b7768bc427d53
Chris Connors <c.connors@gmail.com>
2020-11-01 08:03:18 -0500

    .

--- post_request_server.py
+++ post_request_server.py
@@ -56,12 +56,10 @@ class PostServer:
                 filename = "./static/"+items
                 print(filename)
                 print(type(filename))
-                file = open(filename, 'w')
-                print(type(file))
-                #with open(filename, 'r') as data:
-                #    print(type(data))
-                pickle.dump(data, file)
-                clientsocket.send(data)
+                #file = open(filename, 'w')
+                #print(type(file))
+                with open(filename, 'r') as data:
+                    clientsocket.sendall(data)
                 clientsocket.recv(1024)
                 counter = counter + 1
             
